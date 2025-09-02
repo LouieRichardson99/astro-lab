cd3f726619df1a3a2fe82f76e563f23bc6448df9
Louie Richardson <louie@bumpdigital.co.uk>
2025-09-03 07:46:50 +1000

    Add service for updating component data

--- src/services/component-data.ts
+++ src/services/component-data.ts
@@ -13,3 +13,25 @@ export async function getComponentData(
     return null;
   }
 }
+
+export async function updateComponentData(
+  componentId: string,
+  type: 'prop' | 'slot',
+  name: string,
+  value: any,
+  Astro?: AstroGlobal
+) {
+  const origin = Astro?.url.origin || window.location.origin;
+
+  await fetch(`${origin}/_astrolab/api/component`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      componentId,
+      [type]: name,
+      value
+    })
+  });
+}
